67a3abd45f863b26c5594991cbb05d4d942ec0a9
Galen Reich <galen@bellingcat.com>
2025-06-27 14:07:32 +0100

    User real database for user auth

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,31 +1,84 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
+from contextlib import asynccontextmanager
+import getpass
 
 import jwt
 import bcrypt
-from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi import Depends, APIRouter, HTTPException, status, FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from pydantic import BaseModel
+import sqlite3
+from os import getenv
 
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+DB_PATH = "../data/users.db"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan event handler to create the user database when the app starts.
+    """
+    # Lines here will run when the app starts
+    create_user_database()
+    yield
+    # Lines here will run when the app stops
 
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": b'$2b$12$9KUjGtcbVeRrNzA5i8VS1O57PaOSUrNGdN50PTe3Ejm7FfHTCaAa6', 
-        "disabled": False,
-    }
-}
+router = APIRouter(lifespan=lifespan)
 
-router = APIRouter()
+def create_admin_user() -> tuple[str, str, str, bytes]:
+
+    # Check environment variables for admin user details
+    username = getenv("ADMIN_USERNAME","")
+    password = getenv("ADMIN_PASSWORD","")
+    full_name = getenv("ADMIN_FULL_NAME","")
+    email = getenv("ADMIN_EMAIL","")
+
+    if username and password:
+        hashed_password = get_password_hash(password)
+        return username, full_name, email, hashed_password
+
+    # If environment variables are not set, prompt for input
+    print("No admin user details found in environment variables.")
+    print("Please enter the details for the admin user.")
+    username = input("Enter a username: ")
+    full_name = input("Enter full name: ")
+    email = input("Enter email: ")
+    password = getpass.getpass("Enter password: ")
+    hashed_password = get_password_hash(password)
+
+    if not username or not password:
+        raise ValueError("Username and password are required.")
+    
+    return username, full_name, email, hashed_password
+
+
+def create_user_database()-> None:
+    # Create tables
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                username TEXT PRIMARY KEY,
+                full_name TEXT,
+                email TEXT UNIQUE,
+                hashed_password BLOB,
+                disabled BOOLEAN DEFAULT FALSE
+            )
+        ''')
+        # If the table is empty, create a user
+        cursor = conn.execute('SELECT COUNT(*) FROM users')
+        if cursor.fetchone()[0] == 0:
+            username, full_name, email, hashed_password = create_admin_user()
+            conn.execute('''
+                INSERT INTO users (username, full_name, email, hashed_password)
+                VALUES (?, ?, ?, ?)
+            ''', (username, full_name, email, hashed_password))
 
 class Token(BaseModel):
     access_token: str
@@ -62,14 +115,31 @@ def get_password_hash(password:str) -> bytes:
     return bcrypt.hashpw(password_utf8, salt)
 
 
-def get_user(db, username: str) -> UserInDB | None:
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(username: str) -> UserInDB | None:
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.execute('''
+                SELECT username, full_name, email, hashed_password, disabled FROM users
+                WHERE username = ?
+            ''', (username,))
+
+        user = cursor.fetchone()
+
+    if not user:
+        return None
+    
+    username, full_name, email, hashed_password, disabled = user
+
+    return UserInDB(
+        username=username,
+        full_name=full_name,
+        email=email,
+        hashed_password=hashed_password,
+        disabled=disabled
+    )
 
 
-def authenticate_user(fake_db, username: str, password: str) -> UserInDB | None:
-    user = get_user(fake_db, username)
+def authenticate_user(username: str, password: str) -> UserInDB | None:
+    user = get_user(username)
     if not user:
         return
     if not verify_password(password, user.hashed_password):
@@ -104,7 +174,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> Use
         raise credentials_exception
     if token_data.username is None:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -122,7 +192,7 @@ async def get_current_active_user(
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
