7a8c07fff559dc1cce256d7c0af139300c6b036f
Galen Reich <galen@bellingcat.com>
2025-07-01 16:04:27 +0100

    Refactor meetings logic into new route and db access module

--- api/db/meetings.py
+++ api/db/meetings.py
@@ -5,6 +5,80 @@ from api.models.meetings import MeetingItem
 DB_PATH = "../data/council_meetings.db"
 
 
+def create_database() -> None:
+    # Create tables
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS authorities (
+                id TEXT PRIMARY KEY
+            )
+        """
+        )
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS meetings (
+                uid TEXT PRIMARY KEY,
+                authority TEXT,
+                title TEXT,
+                description TEXT,
+                datetime TEXT,
+                unixtime INTEGER,
+                link TEXT,
+                FOREIGN KEY (authority) REFERENCES authorities(id)
+            )
+        """
+        )
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS transcripts (
+                uid TEXT,
+                transcript TEXT,
+                title TEXT,
+                description TEXT,
+                start_time TEXT,
+                end_time TEXT,
+                PRIMARY KEY (uid, start_time, end_time),
+                FOREIGN KEY (uid) REFERENCES meetings(uid)
+            )
+        """
+        )
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS agenda (
+                uid TEXT,
+                agenda_id TEXT,
+                agenda_text TEXT,
+                agenda_time TEXT,
+                PRIMARY KEY (uid, agenda_id),
+                FOREIGN KEY (uid) REFERENCES meetings(uid)
+            )
+        """
+        )
+        conn.execute(
+            """
+            CREATE VIRTUAL TABLE IF NOT EXISTS transcripts_fts USING fts5(
+                uid,
+                transcript,
+                tokenize='porter'
+            )
+        """
+        )
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS offsets (
+                uid TEXT,
+                offset INTEGER,
+                start_time TEXT,
+                start_time_seconds INTEGER,
+                PRIMARY KEY (uid, offset),
+                FOREIGN KEY (uid) REFERENCES meetings(uid)
+            )
+        """
+        )
+    return
+
+
 def add_meetings_to_db(authority: str, meetings: list[MeetingItem]) -> None:
     """
     Add meetings to the database.
@@ -107,3 +181,168 @@ def add_meetings_to_db(authority: str, meetings: list[MeetingItem]) -> None:
                 """,
                 (uid, full_transcript, uid),
             )
+
+
+def get_transcript_and_meeting_counts(authority: str) -> tuple[int, int]:
+
+    with sqlite3.connect(DB_PATH) as conn:
+        # Compare number of transcripts_fts and meetings
+        cursor = conn.execute(
+            """
+            SELECT COUNT(*) FROM transcripts_fts WHERE uid IN (
+                SELECT uid FROM meetings WHERE authority = ?
+            )
+        """,
+            (authority,),
+        )
+
+        transcripts_count = cursor.fetchone()[0]
+
+        cursor = conn.execute(
+            """
+            SELECT COUNT(*) FROM meetings WHERE authority = ?
+        """,
+            (authority,),
+        )
+
+        meetings_count = cursor.fetchone()[0]
+
+    return transcripts_count, meetings_count
+
+
+def get_authorities_and_transcript_counts() -> dict[str, int]:
+    """
+    Get a list of authorities and their corresponding number of available transcripts.
+    """
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.execute(
+            """
+            SELECT authority, COUNT(*) as transcript_count
+            FROM authorities
+            JOIN meetings ON authorities.id = meetings.authority
+            WHERE meetings.uid IN (
+                SELECT uid FROM transcripts_fts
+            )
+            GROUP BY authority
+        """
+        )
+        authorities = {row[0]: row[1] for row in cursor.fetchall()}
+    return authorities
+
+
+def build_query_string(
+    query: str,
+    authority: list[str] | None = None,
+    startdate: str | None = None,
+    enddate: str | None = None,
+) -> tuple[str, list[str]]:
+
+    params = [query]
+
+    # Search for the phrase in the transcripts using FTS
+    query_string = """
+        SELECT uid, snippet(transcripts_fts, 1, '[', ']', '', 70) AS snippet, rank, transcript
+        FROM transcripts_fts
+        WHERE transcript MATCH ?
+    """
+
+    # Filter by authority if provided
+    if authority:
+        query_string += """
+            AND uid IN (
+                SELECT uid FROM meetings WHERE authority IN ({})
+            )
+        """.format(
+            ",".join("?" for _ in authority)
+        )
+        params.extend(authority)
+
+    # Filter by startdate and enddate if provided
+    if startdate:
+        query_string += """
+            AND uid IN (
+                SELECT uid FROM meetings WHERE datetime >= ?
+            )
+        """
+        params.append(startdate)
+
+    if enddate:
+        query_string += """
+            AND uid IN (
+                SELECT uid FROM meetings WHERE datetime <= ?
+            )
+        """
+        params.append(enddate)
+
+    query_string += " ORDER BY bm25(transcripts_fts)"
+    return query_string, params
+
+
+def search_meetings(
+    query: str,
+    authority: list[str] | None = None,
+    startdate: str | None = None,
+    enddate: str | None = None,
+) -> list[dict]:
+    """
+    Search for meetings based on the query string.
+    Returns a list of dictionaries with meeting details.
+    """
+
+    query_string, params = build_query_string(
+        query=query,
+        authority=authority,
+        startdate=startdate,
+        enddate=enddate,
+    )
+
+    with sqlite3.connect(DB_PATH) as conn:
+        results = conn.execute(query_string, params).fetchall()
+
+        formatted_results = []
+        for result in results:
+            # Calculate the start time from the offset
+            uid, snippet, rank, transcript = result
+            # Use the matched snippet to find the offset in the full transcript
+            snippet_cleaned = snippet.replace("[", "").replace("]", "")
+
+            offset = transcript.find(snippet_cleaned)
+
+            # Get the largest offset less than the current offset
+            cursor = conn.execute(
+                """
+                SELECT start_time, start_time_seconds FROM offsets
+                WHERE uid = ? AND offset <= ? ORDER BY offset DESC LIMIT 1
+            """,
+                (uid, offset),
+            )
+
+            start_time, start_time_seconds = cursor.fetchone()
+
+            # Fetch the link from the meetings table matching the uid
+            meeting_cursor = conn.execute(
+                """
+                SELECT title, datetime, unixtime, link, authority FROM meetings
+                WHERE uid = ?
+            """,
+                (uid,),
+            )
+            meeting_title, datetime, unixtime, meeting_link, authority_result = (
+                meeting_cursor.fetchone()
+            )
+            meeting_link = f"{meeting_link}/start_time/{1000*start_time_seconds}"
+
+            formatted_results.append(
+                {
+                    "title": meeting_title,
+                    "datetime": datetime,
+                    "unixtime": unixtime,
+                    "snippet": snippet,
+                    "start_time": start_time,
+                    "rank": rank,
+                    "link": meeting_link,
+                    "authority": authority_result,
+                }
+            )
+
+        return formatted_results
