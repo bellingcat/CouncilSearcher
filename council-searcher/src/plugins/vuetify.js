cf48176ca245126b33de6163e25c0b485ba8fe58
Galen Reich <galen@bellingcat.com>
2025-08-25 16:03:11 +0100

    Change import method for mdi icons

--- council-searcher/src/plugins/vuetify.js
+++ council-searcher/src/plugins/vuetify.js
@@ -5,17 +5,24 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 // Labs
 import { VDateInput } from 'vuetify/labs/VDateInput'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   theme: {
     defaultTheme: 'dark',
   },
